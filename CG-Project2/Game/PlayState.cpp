2cb417e9dffd10e364975c6cd051f1b03ed213ac
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-07-09 13:43:21 +0200

    added selecting object feature

--- CG-Project2/Game/PlayState.cpp
+++ CG-Project2/Game/PlayState.cpp
@@ -10,10 +10,11 @@
 
 #include "../Scene/Scene.hpp"
 
+#include "../Menu/IGCamera.hpp"
 #include "../Menu/IGEntity.hpp"
 #include "../Menu/IGMenu.hpp"
 #include "../Menu/IGMode.hpp"
-#include "../Menu/IGCamera.hpp"
+#include <algorithm>
 
 PlayState PlayState::playState;
 
@@ -23,11 +24,12 @@ PlaneEntity *plane;
 Cubemap *skybox;
 Shader cubeShader, planeShader, skyboxShader;
 
-Scene s;
+Scene obj_scene;
 
 InputMode user_mode = InputMode::INTERACT;
 
-IGMenu *entityMenu, *modeMenu, *cameraMenu;
+IGMenu *modeMenu, *cameraMenu;
+IGEntity *entityMenu = nullptr;
 
 static Mouse mouse;
 
@@ -50,6 +52,11 @@ void PlayState::init() {
     cube->setCollider(new CubeCollider(cube->getTransform()));
     cube->setDynamic();
 
+    sphere = new Sphere();
+    sphere->createVertexArray();
+    sphere->setScale(vec3(1));
+    sphere->applyTransformation(vec3(0), vec3(1), vec3(0), 0);
+
     planeShader = Shader("./resources/shaders/vertexShader.glsl", "./resources/shaders/fragmentShader.glsl");
     planeShader.use();
     planeShader.setMat4("projection", projection);
@@ -67,11 +74,11 @@ void PlayState::init() {
     skybox->createVertexArray();
 
     // add elements to the scene
-    s.addElement(plane, planeShader);
-    s.addElement(cube, cubeShader);
+    obj_scene.addElement(cube, cubeShader);
+    obj_scene.addElement(sphere, cubeShader);
 
     // imgui
-    entityMenu = new IGEntity(cube);
+    entityMenu = new IGEntity();
     modeMenu = new IGMode(&user_mode);
     cameraMenu = new IGCamera();
 
@@ -86,9 +93,9 @@ void PlayState::resume() {}
 
 GLenum val = GL_FALSE;
 
-void clearMouseFunc(GLFWwindow *window, double x, double y) { ImGui_ImplGlfw_CursorPosCallback(window, x, y); }
+void clearCursorPosFunc(GLFWwindow *window, double x, double y) { ImGui_ImplGlfw_CursorPosCallback(window, x, y); }
 
-void passiveMouseFunc(GLFWwindow *window, double x, double y) {
+void passiveCursorPosFunc(GLFWwindow *window, double x, double y) {
     if (mouse.first_mouse) {
         mouse.first_mouse = false;
         mouse.lastX = x;
@@ -104,6 +111,90 @@ void passiveMouseFunc(GLFWwindow *window, double x, double y) {
     camera.processMouseMovement(xoffset, yoffset);
 
     // ImGui_ImplGlfw_CursorPosCallback(window, x, y);
+}
+
+vec3 getRayFromMouse(int mouse_x, int mouse_y) {
+    mouse_y = HEIGHT - mouse_y;
+
+    float ndc_x = (2.0f * mouse_x) / WIDTH - 1.0f;
+    float ndc_y = (2.0f * mouse_y) / HEIGHT - 1.0f;
+    float ndc_z = -1.0f;
+
+    vec4 clip = vec4(ndc_x, ndc_y, ndc_z, 1.0f);
+
+    vec4 view_model_p = inverse(projection) * clip;
+    view_model_p.w = 1;
+
+    vec4 pw = inverse(camera.getViewMatrix()) * view_model_p;
+
+    return normalize(vec3(pw) - vec3(camera.getCameraPosition()));
+}
+
+bool isRayInSphere(vec3 ray, vec3 sphere_pos, float sphere_radius, float *id) {
+    vec3 d = camera.getCameraPosition() - sphere_pos;
+    float b = dot(d, ray);
+    float cc = dot(d, d) - sphere_radius * sphere_radius;
+    float delta = b * b - cc;
+
+    if (delta < 0.0f)
+        return false;
+    else if (delta > 0.0f) {
+        float t_a = -b + sqrt(delta);
+        float t_b = -b - sqrt(delta);
+        *id = t_b;
+
+        if (t_a < 0.0f && t_b < 0.0f)
+            return false;
+
+        return true;
+    } else { // delta == 0.0f
+        float t = -b + sqrt(delta);
+        if (t < 0)
+            return false;
+        *id = t;
+        return true;
+    }
+}
+
+void selectMouseFunc(GLFWwindow *window, int button, int action, int mod) {
+    // select objects
+    if (button == GLFW_MOUSE_BUTTON_1) {
+        if (action == GLFW_PRESS) {
+            double x, y;
+            glfwGetCursorPos(window, &x, &y);
+            float xmouse = x;
+            float ymouse = y;
+
+            vec3 ray = getRayFromMouse(xmouse, ymouse);
+            // closest_intersection
+            float ci = 0;
+            // this pointer keeps track of the selected object
+            Entity* selected = nullptr;
+            
+            // check all the element of the scene for intersection with ray
+            for(auto elem : obj_scene.getElements()) {
+                auto obj = elem.first;
+                // distance between ray and object
+                float dist = 0.f;
+
+                if (isRayInSphere(ray, obj->getPosition(), 0.7, &dist)) {
+                    if (selected == nullptr || dist <= ci) {
+                        selected = obj;
+                        ci = dist;
+                    }
+                }
+            }
+
+            // updating the observer in IGEntity menu
+            if (selected != nullptr) {
+                entityMenu->changeObserver(selected);
+            } else {
+                entityMenu->changeObserver(nullptr);
+            }
+        }
+    }
+
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mod);
 }
 
 void PlayState::handleEvent(GameEngine *engine) {
@@ -125,17 +216,19 @@ void PlayState::handleEvent(GameEngine *engine) {
     case SELECT:
         // enable selecting object mode
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-        glfwSetCursorPosCallback(window, clearMouseFunc);
+        glfwSetCursorPosCallback(window, clearCursorPosFunc);
+
+        glfwSetMouseButtonCallback(window, selectMouseFunc);
         break;
     case INTERACT:
         // enable movement in the scene, and active mouse movement
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-        glfwSetCursorPosCallback(window, clearMouseFunc);
+        glfwSetCursorPosCallback(window, clearCursorPosFunc);
         break;
     case PASSIVE:
         // enable passive mouse movement
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        glfwSetCursorPosCallback(window, passiveMouseFunc);
+        glfwSetCursorPosCallback(window, passiveCursorPosFunc);
 
         if (glfwRawMouseMotionSupported())
             glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
@@ -198,13 +291,15 @@ void PlayState::update(GameEngine *engine) {
     skyboxShader.setMat4("view", mat4(mat3(camera.getViewMatrix())));
 
     updatePosition(cube);
+    updatePosition(sphere);
 }
 
 void PlayState::draw(GameEngine *engine) {
 
     skybox->draw(skyboxShader);
+    plane->draw(planeShader);
 
-    s.draw();
+    obj_scene.draw();
 
     entityMenu->render();
     modeMenu->render();
