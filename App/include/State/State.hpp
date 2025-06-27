711fba741726253df06c420e2e6526c8e0092612
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2025-06-27 10:03:40 +0200

    documentation for State

--- App/include/State/State.hpp
+++ App/include/State/State.hpp
@@ -3,10 +3,25 @@
 #include <map>
 #include "../../../Opengl-Core/include/Core.hpp"
 
+/**
+ * @brief This class allow to create a state to be attached to the StateManager.
+ * A State works like a scene: defines all the meshes that will be rendered or not,
+ * defines custom behaviour while updating mesh data or while rendering meshes.
+ *
+ * This is an abstract class it needs to be extended and override the methods that
+ * the child State will use.
+ *
+ * It can be used to create scenes with meshes, lights, skybox etc.
+ * A State can have custom behaviour while updating and rendering, every "static"
+ * mesh must be initialized in State::onAttach() implementation.
+ *
+ * @note This application when executed will load a state loader (scene picker),
+ * where the user choose which state (scene) will be loaded.
+ */
 class State {
 public:
 	/**
-	 * This method is called after using `StateManager::sync()` (read `StateMnaager::sync()` documentation for more info).
+	 * @brief This method is called after using `StateManager::sync()` (read `StateMnaager::sync()` documentation for more info).
 	 * It should contain all the operations that must be done before you start the execution of
 	 * a `State`.
 	 *
@@ -18,7 +33,7 @@ public:
 	}
 
 	/**
-	 * This method is called while using `sync()` (read `sync()` documentation for more info).
+	 * @brief This method is called while using `sync()` (read `sync()` documentation for more info).
 	 * It should contain all the operations that must be done at the end of `State` execution
 	 * before deleting or changing `State`, it is automatically called by `sync()`.
 	 *
@@ -30,38 +45,40 @@ public:
 	}
 
 	/**
-	 * This methods contains all the operations executed at every `event::loop::LOOP_UPDATE`
+	 * @brief This methods contains all the operations executed at every `event::loop::LOOP_UPDATE`
 	 * if there are any.
 	 */
 	virtual void onUpdate() {}
 
 	/**
-	 * This methods contains all the operations executed at every `event::loop::LOOP_RENDER`
+	 * @brief This methods contains all the operations executed at every `event::loop::LOOP_RENDER`
 	 * if there are any.
 	 */
 	virtual void onRender() {}
 
 	/**
-	 * Retrieves the current binary state of the current `State`
+	 * @brief Retrieves the current binary state of the current `State`
 	 *
 	 * @return `bool` true if the state is finished
 	 */
 	virtual bool isCurrentStateEnd() { return true; }
 
-	/* Retrieves the `State` name.*
+	/* @brief Retrieves the `State` name.*
 	 *
 	 * @return `std::string` state's name
 	 */
 	std::string getName() const { return this->m_name; }
 
-	/* Retrieves if the state is attached.*
+	/* @brief Retrieves if the state is attached.*
 	 *
 	 * @return `true` if the state is attached
 	 */
 	bool isAttached() const { return this->m_attached; }
 
 	/**
-	 * It needs a name for each State in case you want to use State caching.
+	 * @brief Instances a state with the given name.
+	 *
+	 * @note Each state need a name to be cached in the StateManager.
 	 *
 	 * @params name the state's name
 	 */
@@ -70,10 +87,15 @@ public:
 	virtual ~State() = default;
 
 protected:
+	/// state's name
 	const std::string m_name;
+	/// true if the state is attached
 	bool m_attached = false;
 };
 
+/**
+ * @brief
+ */
 class StateManager {
 public:
 	StateManager(StateManager &other) = delete;
@@ -81,7 +103,7 @@ public:
 	void operator=(const StateManager &other) = delete;
 
 	/**
-	 * Retrieves the instance of the StateManager if it's not created.
+	 * @brief Retrieves the instance of the StateManager if it's not created.
 	 * This function is thread safe using a simple `std::mutex`.
 	 *
 	 * @return `StateManager` unique object.
@@ -95,7 +117,7 @@ public:
 	}
 
 	/**
-	 * Retrieves `true` if the current state should exit.
+	 * @brief Retrieves `true` if the current state should exit.
 	 * If the current state is detached, this will throw an error.
 	 *
 	 * @reutrn `bool`
@@ -106,16 +128,17 @@ public:
 	}
 
 	/**
-	 * Execute `State::onUpdate()` method of the current state.
+	 * @brief Execute `State::onUpdate()` method of the current state.
 	 */
 	void execUpdate() { this->m_currentState->onUpdate(); }
+
 	/**
-	 * Execute `State::onRender()` method of the current state.
+	 * @brief Execute `State::onRender()` method of the current state.
 	 */
 	void execRender() { this->m_currentState->onRender(); }
 
 	/**
-	 * This method works like a barrier: after calling `StateManager::changeState()`,
+	 * @brief This method works like a barrier: after calling `StateManager::changeState()`,
 	 * detach the current state; clear the `EventManager`, attach the queued `State`,
 	 * defines new event listeners.
 	 * It needs to be called after every `StateManager::changeState()` or at the end
@@ -126,12 +149,12 @@ public:
 	void sync();
 
 	/**
-	 * Cleans all the current state cache system.
+	 * @brief Cleans all the current state cache system.
 	 */
 	void clean();
 
 	/**
-	 * Works as the other `StateManager::changeState(const Shared<State>&)` but this
+	 * @brief Works as the other `StateManager::changeState(const Shared<State>&)` but this
 	 * will use the given `name` while caching.
 	 *
 	 * @see StateManager::changeState(const Shared<State>&)
@@ -142,7 +165,7 @@ public:
 	void changeState(const std::string &name, const Shared<State> &state);
 
 	/**
-	 * This method queue the given `State` and tells the `StateManager` that the current
+	 * @brief This method queue the given `State` and tells the `StateManager` that the current
 	 * state needs to be changed.
 	 * If the given `State` is not in the cache system, the `StateManager` will cache the `State`
 	 * with the state's name.
@@ -153,7 +176,7 @@ public:
 	 */
 	inline void changeState(const Shared<State> &state) { this->changeState(state->getName(), state); }
 	/**
-	 * This method search in the cache the `State` with the given name.
+	 * @brief This method search in the cache the `State` with the given name.
 	 * If the name is not found in the cache, the current state will not change.
 	 *
 	 * @param name the state's name
@@ -161,14 +184,14 @@ public:
 	void changeState(const std::string &name);
 
 	/**
-	 * Retrieves if the current state is defined or not.
+	 * @brief Retrieves if the current state is defined or not.
 	 *
 	 * @return true if the current state is initialized
 	 */
 	bool isCurrentStateDefined() const { return this->m_currentState != nullptr; }
 
 	/**
-	 * This method caches the `State` given with the given name.
+	 * @brief This method caches the `State` given with the given name.
 	 *
 	 * @param name state's name
 	 * @param State a shared pointer to the `State`
@@ -177,14 +200,14 @@ public:
 	inline void cacheState(const Shared<State> &state) { this->cacheState(state->getName(), state); }
 
 	/**
-	 * Removes the `State` with the given name if contained in the cache.
+	 * @brief Removes the `State` with the given name if contained in the cache.
 	 *
 	 * @param name state's name
 	 */
 	void uncacheState(const std::string &name);
 
 	/**
-	 * Retrieves the current cache of the `StateManager`.
+	 * @brief Retrieves the current cache of the `StateManager`.
 	 *
 	 * @return cachedStates map of (`std::string`, `Shared<State>`)
 	 */
@@ -193,12 +216,17 @@ public:
 	~StateManager() = default;
 
 private:
+	/// static pointer for Singleton
 	inline static Shared<StateManager> s_pointer = nullptr;
 
+	/// pointer to the current state
 	Shared<State> m_currentState = nullptr;
+	/// pointer to the queued state
 	Shared<State> m_queueState = nullptr;
+	/// map of cached states
 	std::map<std::string, Shared<State>> m_cacheStates{};
 
+	/// true if the state is changed and queued state is not nullptr
 	bool m_stateChanged = false;
 
 	StateManager() = default;
