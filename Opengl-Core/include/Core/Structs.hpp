1cec54c01877501751b52a2c03583ae230567931
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2025-07-16 15:59:41 +0200

    sync

--- Opengl-Core/include/Core/Structs.hpp
+++ Opengl-Core/include/Core/Structs.hpp
@@ -1,27 +1,59 @@
 #pragma once
 
+/**
+ * @brief Simple data structure to store a RGBA color.
+ */
 struct Color {
-    float r = 0.f, g = 0.f, b = 0.f, a = 1.f;
+	/// red channel
+	float r = 0.f;
+	/// green channel
+	float g = 0.f;
+	/// blue channel
+	float b = 0.f;
+	/// alpha channel
+	float a = 1.f;
 };
 
+/**
+ * @brief Data structure to create an ogl::Texture using these parameters.
+ */
 struct TextureParams {
-    unsigned int target = 0;
-    int level = 0;
-    int internalFormat = 0;
-    int border = 0;
-    unsigned int format = 0;
-    unsigned int dataType = 0;
+	/// texture target
+	unsigned int target = 0;
+	/// texture level
+	int level = 0;
+	/// texture internal data format
+	int internalFormat = 0;
+	/// texture border
+	int border = 0;
+	/// texture data format
+	unsigned int format = 0;
+	/// texture data type
+	unsigned int dataType = 0;
 };
 
+/**
+ * @brief Data structure to create an ogl::RenderBuffer using these parameters.
+ */
 struct RBConfig {
-    unsigned int format = 0;
-    unsigned int attachment = 0;
-    unsigned int width = 0;
-    unsigned int height = 0;
+    /// render buffer format
+	unsigned int format = 0;
+    /// render buffer attachment
+	unsigned int attachment = 0;
+    /// render buffer image width
+	unsigned int width = 0;
+    /// render buffer image height
+	unsigned int height = 0;
 };
 
+/**
+ * @brief Data structure to create an ogl::FrameBuffer using these parameters.
+ */
 struct FBConfig {
-    unsigned int operation = 0;
-    unsigned int width = 0;
-    unsigned int height = 0;
+    /// frame buffer operation
+	unsigned int operation = 0;
+    /// frame buffer width
+	unsigned int width = 0;
+    /// frame buffer height
+	unsigned int height = 0;
 };
